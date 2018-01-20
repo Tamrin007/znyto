341d543a8b18ce641465c50619d4ab1611cef9ef
Tamrin007 <tamrin.bitterchococo@gmail.com>
2018-01-20 09:38:37 +0900

    draw graph

--- client/src/components/ZnyTo/Information.vue
+++ client/src/components/ZnyTo/Information.vue
@@ -1,12 +1,53 @@
 <template>
-  
+  <highcharts :options="options"></highcharts>
 </template>
 
 <script>
+function utc2dateString(msec) {
+  const d = new Date();
+  d.setTime(msec);
+  return `${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
+}
+
 export default {
   name: 'Information',
   props: {
     rate: Number,
+  },
+  data() {
+    return {
+      options: {
+        rangeSelector: {
+          selected: 1,
+        },
+        title: {
+          text: 'ZNY to JPY',
+        },
+        series: [{
+          name: 'JPY/ZNY',
+          data: [],
+          tooltip: {
+            valueDecimals: 2,
+          },
+          animation: false,
+        }],
+        xAxis: [{  // X軸
+          labels: {
+            formatter() {
+              return utc2dateString(this.value);
+            },
+          },
+        }],
+      },
+    };
+  },
+  watch: {
+    rate(r) {
+      const now = Date.now();
+      /* eslint-disable no-console */
+      this.options.series[0].data.push([now, r]);
+      console.log(this.options.series[0].data);
+    },
   },
 };
 </script>
--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import VueHighcharts from 'vue-highcharts';
+import Highcharts from 'highcharts';
+import loadStock from 'highcharts/modules/stock';
+import loadHighchartsMore from 'highcharts/highcharts-more';
 import App from './App';
 
 Vue.config.productionTip = false;
+loadStock(Highcharts);
+loadHighchartsMore(Highcharts);
+Vue.use(VueHighcharts, { Highcharts });
 
 /* eslint-disable no-new */
 new Vue({
